7f12d99fc6818d19ccbbb88694af0c5c2ff1817f
Aakib khan <101191261+Aakibgithuber@users.noreply.github.com>
2024-08-22 11:37:29 +0530

    Update Main.tf

--- EC2-terraform/Main.tf
+++ EC2-terraform/Main.tf
@@ -1,1 +1,58 @@
+resource "aws_security_group" "Jenkins-sg" {
+  name        = "Jenkins-Security Group"
+  description = "Open 22,443,80,8080,9000,9100,9090,3000"
 
+  # Define a single ingress rule to allow traffic on all specified ports
+  ingress = [
+    for port in [22, 80, 443, 8080, 9000,9100,9090,3000] : {
+      description      = "TLS from VPC"
+      from_port        = port
+      to_port          = port
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    }
+  ]
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Jenkins-sg"
+  }
+}
+
+
+resource "aws_instance" "web" {
+  ami                    = "ami-0c7217cdde317cfec"
+  instance_type          = "t2.large"
+  key_name               = "my key"
+  vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
+  user_data              = templatefile("./install_jenkins.sh", {})
+
+  tags = {
+    Name = "amazon clone"
+  }
+  root_block_device {
+    volume_size = 30
+  }
+}
+resource "aws_instance" "web2" {
+  ami                    = "ami-0c7217cdde317cfec"
+  instance_type          = "t2.medium"
+  key_name               = "my key"
+  vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
+  tags = {
+    Name = "Monitering via grafana"
+  }
+  root_block_device {
+    volume_size = 30
+  }
+}
